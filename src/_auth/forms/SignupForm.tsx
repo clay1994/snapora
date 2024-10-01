7116504322760f7413ee229cf294ad2f0f360da0
clay1994 <rupasinghekamila@gmail.com>
2024-10-01 23:42:41 +1000

    sign up users, appwrite integration and config cleanups

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -8,6 +8,7 @@ import { useForm } from "react-hook-form";
 import { SignupValidation } from "@/lib/validation";
 import { z } from "zod";
 import Loader from "@/components/shared/Loader";
+import { createUserAccount } from "@/lib/appwrite/api";
 
 const SignupForm = () => { 
   const isLoading = false;
@@ -24,10 +25,13 @@ const SignupForm = () => {
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    // create the user
+    const newUser = await createUserAccount(values);
+    console.log('New user from appwrite: ');
+    console.log(newUser);
+    
+    
   }
 
   return (
